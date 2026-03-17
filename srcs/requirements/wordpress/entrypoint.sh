dcc26c01a335aa04164af57b2e423d2353e91869
Michal Kurek <michalkurek92@gmail.com>
2026-03-17 18:52:48 +0100

    added another user

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -3,20 +3,30 @@ cd /var/www/html
 
 if [ ! -f wp-config.php ]; then
 
-wp core download --allow-root
-wp config create \
-    --dbname=$WORDPRESS_DB_NAME \
-    --dbuser=$WORDPRESS_DB_USER \
-    --dbpass=$WORDPRESS_DB_PASSWORD \
-    --dbhost=$WORDPRESS_DB_HOST \
-    --allow-root
-wp core install \
-    --url=$WORDPRESS_URL \
-    --title=$WORDPRESS_TITLE \
-    --admin_user=$WORDPRESS_ADMIN \
-    --admin_password=$WORDPRESS_ADMIN_PASSWORD \
-    --admin_email=$WORDPRESS_ADMIN_EMAIL \
-    --allow-root
+    wp core download --allow-root
+    wp config create \
+        --dbname=$WORDPRESS_DB_NAME \
+        --dbuser=$WORDPRESS_DB_USER \
+        --dbpass=$WORDPRESS_DB_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --allow-root
+        
+    wp core install \
+        --url=$WORDPRESS_URL \
+        --title=$WORDPRESS_TITLE \
+        --admin_user=$WORDPRESS_ADMIN \
+        --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+        --admin_email=$WORDPRESS_ADMIN_EMAIL \
+        --allow-root
+
+    if ! wp user get $WORDPRESS_USER --allow-root > /dev/null 2>&1; then
+		wp user create \
+			$WORDPRESS_USER \
+			$WORDPRESS_USER_EMAIL \
+			--user_pass=$WORDPRESS_USER_PASSWORD \
+			--role=author \
+			--allow-root
+	fi
 fi
 
 php-fpm82 -F
